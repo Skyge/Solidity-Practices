cfef7688f4ff0da6085d5dabd135757eea42b2f7
Skyge <1506186404li@gmail.com>
2024-11-23 14:09:19 +0800

    :safety_vest: Add more test cases to increase coverage for week2.

--- RareSkills/RareSkills-Solidity-Bootcamp/Week2/contracts/test/NFTTrioTest.t.sol
+++ RareSkills/RareSkills-Solidity-Bootcamp/Week2/contracts/test/NFTTrioTest.t.sol
@@ -22,6 +22,14 @@ contract NFTTrioTest is Test {
         discountAccount1 = helperConfig.getConfig().discountAccount1;
 
         vm.deal(alice, 1000 ether);
+        vm.deal(discountAccount1, 1000 ether);
+    }
+
+    // Revert when deploy contract if royalty fee recipient is zero address
+    function testDeployWillFailRoyaltyFeeRecipientIsZeroAddress() external {
+        // Deploy failed
+        vm.expectRevert(MyNFT.RoyaltyFeeRecipientIsZeroAddress.selector);
+        new MyNFT(address(0), bytes32(0));
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -37,9 +45,28 @@ contract NFTTrioTest is Test {
         assert(address(nft).balance == 0.1 ether);
     }
 
-    function testMintWithDiscount() external {
-        vm.deal(discountAccount1, 1000 ether);
+    // Revert when minting if mint limit reached
+    function testMintWillFailMintLimitReached() external {
+        // Mint NFTs to reach the limit
+        uint256 maxSupply = nft.MAX_SUPPLY();
+        for (uint256 i; i < maxSupply; ++i) {
+            nft.safeMint{value: 0.1 ether}(alice);
+        }
 
+        // Mint limit reached
+        vm.expectRevert(MyNFT.MintLimitReached.selector);
+        nft.safeMint{value: 0.1 ether}(alice);
+    }
+
+    // Revert when minting if insufficient payment
+    function testMintWillFailInsufficientPayment() external {
+        uint256 mintPrice = nft.PRICE();
+        // Insufficient payment
+        vm.expectRevert(MyNFT.InsufficientPayment.selector);
+        nft.safeMint{value: mintPrice - 1}(alice);
+    }
+
+    function testMintWithDiscount() external {
         bytes32[] memory proof = new bytes32[](4);
         proof[0] = 0x208697df1b2d4c083944c10909fe1ed6e99c1eaccff33ba129464b28f8245f01;
         proof[1] = 0x6cc962fe200062ec5506465bba79ef2a025aa9e256958d797023b191d2753272;
@@ -61,6 +88,60 @@ contract NFTTrioTest is Test {
         assert(address(nft).balance == 0.08 ether);
     }
 
+    // Revert when minting with discount if mint limit reached
+    function testMintWithDiscountWillFailMintLimitReached() external {
+        // Mint NFTs to reach the limit
+        uint256 maxSupply = nft.MAX_SUPPLY();
+        for (uint256 i; i < maxSupply; ++i) {
+            nft.safeMint{value: 0.1 ether}(alice);
+        }
+
+        bytes32[] memory proof = new bytes32[](0);
+
+        // Mint limit reached
+        vm.expectRevert(MyNFT.MintLimitReached.selector);
+        nft.safeMintWithDiscount{value: 0.08e18}(proof, discountAccount1);
+    }
+
+    // Revert when minting with discount if has claimed by discount
+    function testMintWithDiscountWillFailHasClaimedByDiscount() external {
+        bytes32[] memory proof = new bytes32[](4);
+        proof[0] = 0x208697df1b2d4c083944c10909fe1ed6e99c1eaccff33ba129464b28f8245f01;
+        proof[1] = 0x6cc962fe200062ec5506465bba79ef2a025aa9e256958d797023b191d2753272;
+        proof[2] = 0x1823bf464e3889ced384fe32a5a1543450346e714b7b4d2f9354a1709feca014;
+        proof[3] = 0x2a3cb55154243e3bf9cfbb98f8ed87dd65f0f3f3c1e95e4a9e746c3b2b09d6a6;
+
+        // Has claimed with discount
+        vm.startPrank(discountAccount1);
+        nft.safeMintWithDiscount{value: 0.08e18}(proof, discountAccount1);
+        vm.expectRevert(MyNFT.HasClaimedByDiscount.selector);
+        nft.safeMintWithDiscount{value: 0.08e18}(proof, discountAccount1);
+        vm.stopPrank();
+    }
+
+    // Revert when minting with discount if insufficient payment
+    function testMintWithDiscountWillFailInsufficientPayment() external {
+        uint256 discountPrice = nft.DISCOUNT_PRICE();
+        bytes32[] memory proof = new bytes32[](4);
+        proof[0] = 0x208697df1b2d4c083944c10909fe1ed6e99c1eaccff33ba129464b28f8245f01;
+        proof[1] = 0x6cc962fe200062ec5506465bba79ef2a025aa9e256958d797023b191d2753272;
+        proof[2] = 0x1823bf464e3889ced384fe32a5a1543450346e714b7b4d2f9354a1709feca014;
+        proof[3] = 0x2a3cb55154243e3bf9cfbb98f8ed87dd65f0f3f3c1e95e4a9e746c3b2b09d6a6;
+
+        // Insufficient payment
+        vm.expectRevert(MyNFT.InsufficientPayment.selector);
+        nft.safeMintWithDiscount{value: discountPrice - 1}(proof, discountAccount1);
+    }
+
+    // Revert when minting with discount if proof is invalid
+    function testMintWithDiscountWillFailInvalidProof() external {
+        bytes32[] memory proof = new bytes32[](0);
+
+        // Invalid proof
+        vm.expectRevert(MyNFT.InvalidProof.selector);
+        nft.safeMintWithDiscount{value: 0.08e18}(proof, alice);
+    }
+
     function testWithdrawFunds() external {
         vm.prank(alice);
         nft.safeMint{value: 0.1 ether}(alice);
@@ -72,9 +153,47 @@ contract NFTTrioTest is Test {
         assert(address(nft).balance == 0);
     }
 
+    // Revert when withdrawing funds if not owner
+    function testWithdrawFundsWillFailNotOwner() external {
+        // Alice is not the owner
+        assertTrue(nft.owner() != alice);
+        vm.startPrank(alice);
+        vm.expectRevert();
+        nft.withdrawFunds(alice);
+        vm.stopPrank();
+    }
+
+    // Revert when withdrawing funds if failed
+    function testWithdrawFundsWillFailFailed() external {
+        vm.prank(alice);
+        nft.safeMint{value: 0.1 ether}(alice);
+        assert(address(nft).balance == 0.1 ether);
+
+        address owner = nft.owner();
+        vm.startPrank(owner);
+        vm.expectRevert(abi.encodeWithSelector(MyNFT.WithdrawFundsFailed.selector));
+        // This contract does not have a function to receive ether, so it will revert
+        nft.withdrawFunds(address(this));
+        vm.stopPrank();
+    }
+
     /*//////////////////////////////////////////////////////////////
                            NFTStaking Tests
     //////////////////////////////////////////////////////////////*/
+    // Revert when deploying contract if NFT address is zero address
+    function testDeployWillFailNFTAddressIsZeroAddress() external {
+        // Deploy failed
+        vm.expectRevert(NFTStaking.NFTAddressIsZeroAddress.selector);
+        new NFTStaking(address(0), address(rewardToken));
+    }
+
+    // Revert when deploying contract if reward token address is zero address
+    function testDeployWillFailRewardTokenAddressIsZeroAddress() external {
+        // Deploy failed
+        vm.expectRevert(NFTStaking.RewardTokenAddressIsZeroAddress.selector);
+        new NFTStaking(address(nft), address(0));
+    }
+
     function testStakeNFT() public {
         vm.startPrank(alice);
         nft.safeMint{value: 0.1 ether}(alice);
@@ -86,6 +205,26 @@ contract NFTTrioTest is Test {
         (address tokenIdOwner,) = nftStaking.tokenIdToNFTInfo(tokenId);
         assert(tokenIdOwner == alice);
         assert(nftStaking.getStakedTokenIds(alice).length == 1);
+    }
+
+    // Revert when staking NFT if caller is not the NFT
+    function testStakeNFTWillFailNotOwner() external {
+        vm.startPrank(alice);
+        vm.expectRevert(abi.encodeWithSelector(NFTStaking.CallerIsNotTheNFT.selector));
+        nftStaking.onERC721Received(alice, alice, 1, "");
+        vm.stopPrank();
+    }
+
+    // Revert when staking the same NFT twice
+    function testStakeNFTWillFailAlreadyStaked() external {
+        testStakeNFT();
+        uint256 tokenId = nft.currentTokenId();
+        (address tokenIdOwner,) = nftStaking.tokenIdToNFTInfo(tokenId);
+
+        vm.startPrank(address(nft));
+        vm.expectRevert(abi.encodeWithSelector(NFTStaking.FailedToAddTokenIdToAccount.selector));
+        nftStaking.onERC721Received(tokenIdOwner, tokenIdOwner, tokenId, "");
+        vm.stopPrank();
     }
 
     function testClaimRewards() public {
@@ -102,16 +241,56 @@ contract NFTTrioTest is Test {
         assert(rewardToken.balanceOf(tokenIdOwner) == 10e18);
     }
 
-    function testWithdrawNFT() external {
-        testClaimRewards();
+    // Revert when claiming rewards if caller is not the owner of the NFT
+    function testClaimRewardsWillFailNotOwner() external {
+        testStakeNFT();
         uint256 tokenId = nft.currentTokenId();
         (address tokenIdOwner,) = nftStaking.tokenIdToNFTInfo(tokenId);
+
+        vm.startPrank(makeAddr("NotNFTOwner"));
+        vm.expectRevert(abi.encodeWithSelector(NFTStaking.NotOwnerOfNFT.selector));
+        nftStaking.claim(tokenId);
+        vm.stopPrank();
+    }
+
+    function testWithdrawNFT() external {
+        testStakeNFT();
+        uint256 tokenId = nft.currentTokenId();
+        (address tokenIdOwner,) = nftStaking.tokenIdToNFTInfo(tokenId);
+
+        // Pass 1 day to get rewards
+        vm.warp(block.timestamp + 1 days);
+        uint256 beforeRewardTokenBalance = rewardToken.balanceOf(tokenIdOwner);
 
         vm.startPrank(tokenIdOwner);
         nftStaking.withdrawNFT(tokenId);
         vm.stopPrank();
 
+        uint256 dailyRewardAmount = nftStaking.DAILY_REWARD_AMOUNT();
+        uint256 afterRewardTokenBalance = rewardToken.balanceOf(tokenIdOwner);
+        assert(afterRewardTokenBalance == beforeRewardTokenBalance + dailyRewardAmount);
+
         assert(nftStaking.getStakedTokenIds(tokenIdOwner).length == 0);
         assert(nft.ownerOf(tokenId) == tokenIdOwner);
+    }
+
+    // Revert when withdrawing NFT if caller is not the owner of the NFT
+    function testWithdrawNFTWillFailNotOwner() external {
+        testStakeNFT();
+        uint256 tokenId = nft.currentTokenId();
+        (address tokenIdOwner,) = nftStaking.tokenIdToNFTInfo(tokenId);
+
+        vm.startPrank(makeAddr("NotNFTOwner"));
+        vm.expectRevert(abi.encodeWithSelector(NFTStaking.NotOwnerOfNFT.selector));
+        nftStaking.withdrawNFT(tokenId);
+        vm.stopPrank();
+    }
+
+    // Revert when withdrawing NFT if failed
+    function testWithdrawNFTWillFailFailed() external {
+        vm.startPrank(address(0));
+        vm.expectRevert(abi.encodeWithSelector(NFTStaking.FailedToRemoveTokenIdFromAccount.selector));
+        nftStaking.withdrawNFT(111);
+        vm.stopPrank();
     }
 }
